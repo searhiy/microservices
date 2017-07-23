38c5f44d237225029896543c01ace7b4f4d60546
Serhii Kartashov <serhii.kartashov@eleks.com>
2017-07-23 19:09:52 +0300

    init commit

---
+++ eureka-server/src/main/java/com/example/microservices/Launcher.java
@@ -0,0 +1,40 @@
+package com.example.microservices;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+@Slf4j
+@SpringBootApplication
+@EnableEurekaServer
+@EnableEurekaClient
+@EnableAdminServer
+public class Launcher {
+
+	public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext run = SpringApplication.run(Launcher.class, args);
+
+        Environment env = run.getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://127.0.0.1:{}\n\t" +
+                        "Admin: \t\thttp://127.0.0.1:{}/admin\n\t" +
+                        "External: \thttp://{}:{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+        log.debug("Running with Spring profile(s) : {}", Arrays.toString(env.getActiveProfiles()));
+	}
+}
---
+++ patient-service/src/main/java/com/example/microservices/Launcher.java
@@ -0,0 +1,34 @@
+package com.example.microservices;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+@Slf4j
+@SpringBootApplication
+@EnableEurekaClient
+public class Launcher {
+
+	public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext run = SpringApplication.run(Launcher.class, args);
+
+        Environment env = run.getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://127.0.0.1:{}\n\t" +
+                        "Admin: \t\thttp://127.0.0.1:{}/admin\n\t" +
+                        "External: \thttp://{}:{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+        log.debug("Running with Spring profile(s) : {}", Arrays.toString(env.getActiveProfiles()));
+	}
+}
